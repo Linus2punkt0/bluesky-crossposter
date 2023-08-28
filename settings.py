f567ee388336c58ec58a52f971615d788d733ecc
linus <linus@laserjesus.se>
2023-08-28 13:45:19 +0200

    removed values from settings

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ postDefault = True
 # and Twitter. You can have both the actual language of the tweet, and the selector language
 # added to the tweet and it will still work.
 # Accepted values: Any language tag in quotes (https://en.wikipedia.org/wiki/IETF_language_tag)
-mastodonLang = "da"
-twitterLang = "no"
+mastodonLang = ""
+twitterLang = ""
 # Sets maximum amount of times poster will retry a failed crosspost.
 maxRetries = 5
