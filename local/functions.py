0693251aab57d26656c2d01e238c52383893ac57
linus <linus@laserjesus.se>
2025-02-19 13:55:01 +0100

    Fix issues with logger after update

--- local/functions.py
+++ local/functions.py
@@ -11,7 +11,7 @@ import  os, shutil, re, arrow, sys
 logger.remove()
 log_format = "<yellow>{time:YYYY-MM-DD HH:mm:ss}</yellow> <lvl>[{level}]: {message}</lvl> <yellow>({function} {file}:{line})</yellow>"
 logger.add(sys.stdout, format=log_format, level=settings.log_level)
-logger.add("%s/kamrat_{time:YYMMDD}.log" % log_path,
+logger.add("%s/crossposter_{time:YYMMDD}.log" % log_path,
         level=settings.log_level,
         format=log_format, 
         rotation="00:00", retention="1 week")
